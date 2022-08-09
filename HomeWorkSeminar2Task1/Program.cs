f8d32ee7e0f25d691205c47b3784d083fa5ea689
Rumyanceva <nj89.89@mail.ru>
2022-08-09 09:22:06 +0300

    Add HomeWorkSeminar2Task1

---
+++ HomeWorkSeminar2Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на 
+выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int randomNumber = new Random().Next(100, 1000);
+int remainder = randomNumber % 100;
+int secondNumber = remainder / 10;
+
+Console.WriteLine($"Вторым числом числа {randomNumber} является число {secondNumber}");
