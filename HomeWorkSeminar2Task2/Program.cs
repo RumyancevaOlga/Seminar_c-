02c3167a48422f0ef30e91935a3f2023149207df
Rumyanceva <nj89.89@mail.ru>
2022-08-09 10:33:39 +0300

    Add HomeWorkSeminar2Task2

---
+++ HomeWorkSeminar2Task2/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number < 100 && number > -100)
+{
+    Console.WriteLine($"У числа {number} нет третьей цифры");
+}
+else 
+{
+    int devision = number / 100;
+    int score = 0;
+    while(devision > 1)
+    {
+       score++;
+       devision = devision / 10;
+    }
+     int thirdNumber = 0;
+     if(score > 1)
+     {
+        int degree = 10;
+        int i = 1;
+       while(i < score)  
+       {
+        thirdNumber = number / degree % 10;
+        degree = degree * 10;
+        i++;
+       }
+     }
+       else
+       {
+        thirdNumber = number % 10;
+       }
+       if(thirdNumber > 0)
+       {
+        Console.WriteLine($"Третей цифрой числа {number} является {thirdNumber}");
+       }
+       else
+       {
+        Console.WriteLine($"Третей цифрой числа {number} является {-thirdNumber}");
+       }
+}
