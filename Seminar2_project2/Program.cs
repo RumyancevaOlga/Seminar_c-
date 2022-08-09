ec9e7c17ed4d56d0256850d274fc541cb14c01ad
Rumyanceva <nj89.89@mail.ru>
2022-08-09 09:08:13 +0300

    Add Seminar2...

---
+++ Seminar2_project2/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+11. Напишите программу, которая выводит случайное трехзначное число
+и удаляет вторую цифру этого числа
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+int randomNumber = new Random().Next(100, 1000);
+int firstNumber = randomNumber / 100;
+int thirdNumber = randomNumber % 10;
+int number = firstNumber * 10 + thirdNumber;
+
+Console.WriteLine($"Сгенерированное число {randomNumber}. Получившееся число {number}.");
---
+++ Seminar2_project4/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+14. Напишите программу, которая принимает на вход число и проверяет, 
+кратно ли оно одновременно 7 и 23.
+14 -> нет 
+46 -> нет 
+161 -> да
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int divisionOnSeven = number % 7;
+int divisionOnTwentyThree = number % 23;
+
+if(divisionOnSeven == 0 && divisionOnTwentyThree == 0)
+{
+   Console.WriteLine($"Число {number} делится на 7 и на 23 без остатка"); 
+}
+else
+{
+     Console.WriteLine($"Число {number} НЕ делится на 7 и на 23 без остатка"); 
+}
