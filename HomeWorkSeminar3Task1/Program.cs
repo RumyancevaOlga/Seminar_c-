d2f7b9ec3a2168d2ca676f5e597c6e24b1ef73e6
Rumyanceva <nj89.89@mail.ru>
2022-08-12 13:52:01 +0300

    Add HomeWorkSeminar3Task1

---
+++ HomeWorkSeminar3Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+
+12821 -> да
+
+23432 -> да
+*/
+
+Console.WriteLine("Введите число:");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int lastNumber = 0; 
+int firstNumber = 0;
+int palindrome = userNumber;
+while(palindrome > 9)
+{
+    lastNumber = palindrome % 10;
+    firstNumber = palindrome;
+    int i = 1;
+    while(firstNumber > 9)
+    {
+        firstNumber = firstNumber / 10;
+        i = i * 10;
+    }
+        if (firstNumber != lastNumber)
+        {
+            break;
+        }  
+    palindrome = (palindrome - firstNumber * i) / 10;
+}
+
+if(firstNumber == lastNumber)
+{
+    Console.WriteLine($"Ура! {userNumber} является палиндромом.");
+}
+else
+{
+   Console.WriteLine($"{userNumber} НЕ является палиндромом.");
+}
