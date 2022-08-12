64c4347a020a77f18f727afed62a854eca7b9e4e
Rumyanceva <nj89.89@mail.ru>
2022-08-12 15:26:34 +0300

    Add new contion

--- HomeWorkSeminar3Task1/Program.cs
+++ HomeWorkSeminar3Task1/Program.cs
@@ -33,7 +33,7 @@ while(palindrome > 9)
     palindrome = (palindrome - firstNumber * i) / 10;
 }
 
-if(firstNumber == lastNumber && firstNumber != 0 && lastNumber != 0)
+if(firstNumber == lastNumber && userNumber > 9)
 {
     Console.WriteLine($"Ура! {userNumber} является палиндромом.");
 }
