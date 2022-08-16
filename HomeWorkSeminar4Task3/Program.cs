9df19c6d5ff3f58c6bb39d9fb4c506ffd98a9061
Rumyanceva <nj89.89@mail.ru>
2022-08-16 09:50:37 +0300

    Add HomeWorkSeminar4Task3

---
+++ HomeWorkSeminar4Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+/* поскольку не сказано каким образом задается массив, а также не указан 
+диапазон значений, допустим, что массив задается рандомайзером с диапазоном
+от 1 до 99. */
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{col[position]} ");
+        position++;
+    }
+
+}
+
+int [] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+
+
