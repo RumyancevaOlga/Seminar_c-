714e1076f00e9ada517cd67b4b7b31633afd3c6a
Rumyanceva <nj89.89@mail.ru>
2022-08-16 09:37:32 +0300

    Add HomeWorkSeminar4Task2

---
+++ HomeWorkSeminar4Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+/* Нужно ввести цикл, который будет аккумулировать в себе остаток от деления на 10, 
+при этом само число будет делиться на 10, столько раз, пока число больше 
+нуля */
+
+Console.WriteLine("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+int number = userNumber;
+
+while(number > 0)
+{
+    sum = sum + number % 10;
+    number = number / 10;
+    Console.WriteLine(sum);
+}
+
+Console.WriteLine($"Сумма всех цифр числа {userNumber} равна {sum}");
